43bc5a57a90771715fd3cc143d6701fab53e73ec
bhorlarh <bhorlarinwah@gmail.com>
2021-11-12 14:17:52 +0100

    ft: updated middlewares

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,11 @@ import { nanoid } from 'nanoid';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.post('/api/partners', (req, res) => {
   const { firstname, lastname, shopname, shopemailaddress } = req.body;
